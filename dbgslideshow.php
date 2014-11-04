237778f3d4d5cd70bc1b7d233b98b4ad5257f0fe
dejwid <dawid.paszko@gmail.com>
2014-11-04 14:38:58 +0100

    Update dbgslideshow.php
    maximize image

--- dbgslideshow.php
+++ dbgslideshow.php
@@ -42,7 +42,7 @@ function dbgslideshow_init(){
     </script>
     <style>
         #dbgslideshow,.slideshow{top:0;bottom:0;left:0;right:0;position:fixed;}
-        
+        .slideshow-slide{background-size:cover;}
     </style>
     <?php
     }
